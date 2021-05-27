52542f753e9e36148674e7c6e0d149c937103581
Jakub Lenz <jakublenz7@gmail.com>
2021-05-27 18:41:22 +0200

    Delay with intervals

--- timers.js
+++ timers.js
@@ -1,6 +1,17 @@
 const waitTime = 9000;
+const waitInterval = 500;
+let currentTime = 0;
+
+const incTime = () => {
+    currentTime += waitInterval;
+    console.log(`waiting ${currentTime / 1000} seconds`);
+};
 console.log(`setting a ${waitTime / 1000} second delay`);
 
-const timerFinished = () => console.log("done");
+const timerFinished = () => {
+    clearInterval(interval);
+    console.log("done");
+    };
 
+const interval = setInterval(incTime, waitInterval);
 setTimeout(timerFinished, waitTime);
